e7e05d256a3a3b9fd802e41eabb61f72ba77d854
Unknown <thomaspoint@gmail.com>
2018-02-16 10:05:21 +0100

    Navbar

---
+++ make-it/src/App/Header/HeaderComponent/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="navbar">
+                <ul>
+                    <li><NavLink activeClassName="selected" to='/Tracks'>Submit track</NavLink></li>
+                    <li><NavLink activeClassName="selected" to='/CreateAccount'>Create an account</NavLink></li>
+                    <li><NavLink activeClassName="selected" to='/Works'>How it works</NavLink></li>
+                    <li><NavLink activeClassName="selected" to='/Pricing'>Pricing</NavLink></li>
+                    <li><NavLink activeClassName="selected" to='/Login'>Log in | Sign in</NavLink></li>
+                </ul>
+            </div>
+        )
+    }
+}
--- make-it/src/App/Router/Routes.js
+++ make-it/src/App/Router/Routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Logo from '../Header/HeaderComponent/Logo';
 import CreateAccount from '../Layout/CreateAccount/CreateAccount';
 import Pricing from '../Layout/Pricing/Pircing';
-import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Track from "../Layout/SubmitTrack/Track";
 import Work from "../Layout/Works/Work";
 import Login from "../Layout/Login/Login";
+import Navbar from "../Header/HeaderComponent/Navbar";
 
 export default class Routes extends React.Component {
     render() {
@@ -14,15 +15,7 @@ export default class Routes extends React.Component {
                 <div>
                     <div className="header">
                         <Logo/>
-                        <div className="navbar">
-                            <ul>
-                                <li><NavLink activeClassName="selected" to='/Tracks'>Submit track</NavLink></li>
-                                <li><NavLink activeClassName="selected" to='/CreateAccount'>Create an account</NavLink></li>
-                                <li><NavLink activeClassName="selected" to='/Works'>How it works</NavLink></li>
-                                <li><NavLink activeClassName="selected" to='/Pricing'>Pricing</NavLink></li>
-                                <li><NavLink activeClassName="selected" to='/Login'>Log in | Sign in</NavLink></li>
-                            </ul>
-                        </div>
+                        <Navbar/>
                     </div>
                     <div className="content">
                         <Switch>
