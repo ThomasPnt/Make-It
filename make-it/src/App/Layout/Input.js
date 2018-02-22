8af2f47c37482474f11cf91d2d78fc27cace0530
Unknown <thomaspoint@gmail.com>
2018-02-22 14:41:24 +0100

    scss

--- make-it/src/App/Layout/Input.js
+++ make-it/src/App/Layout/Input.js
@@ -4,6 +4,7 @@ export default class Input extends React.Component {
     render() {
         const { name , input , type , placeholder , className , meta : { touched, error }} = this.props;
         return (
+
             <div className="InputForm">
                 <input {...input} className={className} name={name} type={type} placeholder={placeholder}/>
                 {touched && error && <span id="error">{error}</span>}
